b06af8aab5e4980b1f83166f8e9feefad3f5513c
TBoileau <t-boileau@email.com>
2021-10-06 09:48:37 +0200

    feat: implement Serializable interface

---
+++ src/serializer/serializable.py
@@ -0,0 +1,15 @@
+"""Imported modules/packages"""
+from abc import ABC
+from typing import Dict, Any
+
+
+class Serializable(ABC):
+    """
+    Serializable interface
+    """
+
+    def serialize(self) -> Dict[str, Any]:
+        """
+        Return serialized data
+        :return:
+        """
