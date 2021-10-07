ebe97301372aa02d60e8fe4011ced37fcca867a2
TBoileau <t-boileau@email.com>
2021-10-07 18:30:40 +0200

    feat: quit (#31)

--- src/app.py
+++ src/app.py
@@ -4,7 +4,13 @@ import os
 from dotenv import load_dotenv
 
 from src.controller.app_controller import AppController
-from src.templating.templating import Templating, TemplatingInterface
+from src.controller.tournament_controller import TournamentController
+from src.dependency_injection.container import ContainerInterface, Container
+from src.router.route import Route
+from src.router.router import Router
+from src.router.router_interface import RouterInterface
+from src.templating.templating import Templating
+from src.templating.templating_interface import TemplatingInterface
 
 
 class App:
@@ -13,10 +19,7 @@ class App:
     """
 
     def __init__(self):
-        """
-        Constructor
-        """
-        self.__templates_folder: str = os.path.join(os.getcwd(), "templates")
+        self.__container: ContainerInterface = Container()
 
     def run(self):
         """
@@ -24,10 +27,30 @@ class App:
 
         :return:
         """
+
         load_dotenv(".env")
+        self.build()
+        self.routing()
+        self.__container.get(Router).generate("app_home")
 
-        templating: TemplatingInterface = Templating(self.__templates_folder)
+    def build(self):
+        """
+        Build container
 
-        while True:
-            app_controller: AppController = AppController(templating)
-            app_controller.home()
+        :return:
+        """
+
+        self.__container.set_parameter("templating_directory", os.path.join(os.getcwd(), "templates")).alias(
+            TemplatingInterface, Templating
+        ).alias(RouterInterface, Router)
+
+    def routing(self):
+        """
+        Define routes
+
+        :return:
+        """
+
+        self.__container.get(RouterInterface).add(Route("app_home", AppController, "home")).add(
+            Route("app_quit", AppController, "quit")
+        ).add(Route("tournament_create", TournamentController, "create"))
--- src/controller/app_controller.py
+++ src/controller/app_controller.py
@@ -1,5 +1,10 @@
 """Imported modules/packages"""
+import re
+import sys
+import time
+
 from src.controller.abstract_controller import AbstractController
+from src.input.input import Input
 
 
 class AppController(AbstractController):
@@ -7,11 +12,30 @@ class AppController(AbstractController):
     App controller
     """
 
+    def quit(self):
+        """
+        Quit program
+
+        :return:
+        """
+        self.render("app/quit")
+        time.sleep(3)
+        sys.exit()
+
     def home(self):
         """
         Home
 
         :return:
         """
-        self._render("home")
-        self.redirect("Que souhaitez-vous faire ?", {0: exit})
+        home_input: Input = Input(
+            label="Que souhaitez-vous faire ? ",
+            message="Veuillez saisir un chiffre entre 0 et 2.",
+            transform=int,
+            validate=lambda raw_data: re.match(r"^\d+$", raw_data) and int(raw_data) in [0, 1, 2],
+        )
+        self._choice(
+            home_input,
+            {0: lambda: self.redirect("app_quit"), 1: lambda: self.redirect("tournament_create")},
+            "app/home",
+        )
