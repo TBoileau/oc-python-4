9fefe454bdca0c6ab36a7ac4c1a51d72216499ed
TBoileau <t-boileau@email.com>
2021-10-08 15:34:10 +0200

    feat: workflow (#42)

---
+++ src/workflow/subject.py
@@ -0,0 +1,32 @@
+"""Imported modules/packages"""
+from abc import ABC
+from typing import Dict
+
+from src.workflow.transition import Transition
+
+
+class Subject(ABC):
+    """
+    Subject interface
+    """
+
+    def get_state(self) -> str:
+        """
+        Get state
+
+        :return:
+        """
+
+    def set_state(self, state: str):
+        """
+        Set state
+
+        :return:
+        """
+
+    def get_transitions(self) -> Dict[str, Transition]:
+        """
+        Get transitions
+
+        :return:
+        """
---
+++ src/workflow/workflow.py
@@ -0,0 +1,22 @@
+"""Imported modules/packages"""
+from src.workflow.subject import Subject
+from src.workflow.transition import Transition
+from src.workflow.workflow_interface import WorkflowInterface
+
+
+class Workflow(WorkflowInterface):
+    """
+    Workflow
+    """
+
+    def can(self, subject: Subject, transition_name: str) -> bool:
+        assert transition_name in subject.get_transitions()
+        transition: Transition = subject.get_transitions()[transition_name]
+        return subject.get_state() in transition.from_states and (transition.guard is None or transition.guard(subject))
+
+    def apply(self, subject: Subject, transition_name: str):
+        assert self.can(subject, transition_name)
+        transition: Transition = subject.get_transitions()[transition_name]
+        subject.set_state(transition.to_state)
+        if transition.completed is not None:
+            transition.completed(subject)
